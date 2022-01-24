061299d8379e9531f07bc2f5c1609ea7c1f986ef
edorich <hendro.hudaya@gmail.com>
2022-01-24 16:40:13 +0700

    Added mvc to application

--- edo.BookStore/edo.BookStore/Startup.cs
+++ edo.BookStore/edo.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace edo.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,10 +56,7 @@ namespace edo.BookStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.Map("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
